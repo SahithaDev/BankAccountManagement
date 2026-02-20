4508da36aa7d608049362353db112a6c357db607
Sahitha0203 <sahitha.mutakodoor@gmail.com>
2026-02-20 23:00:21 +0530

    implemented multiple  inheritance

--- OOP2.js
+++ OOP2.js
@@ -15,7 +15,15 @@ class square extends Box {
     this.weight = weight;
   }
 }
+class BoxPrice extends square {
+  constructor(l, w, h, weight, price) {
+    super(l, w, h, weight);
+    this.price = price;
+  }
+}
 const newBox = new square(2, 2, 2, 5);
 const newBox1 = new Box(2, 2, 2);
+const newBox2 = new BoxPrice(2, 2, 2, 5, 200);
 console.log(newBox);
+console.log(newBox2);
 console.log(newBox1);
